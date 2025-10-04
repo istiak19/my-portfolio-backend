4bd79cd0782bdbf95af5cba6e2d459fa52ad04b1
istiak19 <istiak15-14128@diu.edu.bd>
2025-10-04 21:49:09 +0600

    cookies updated

--- src/app/utils/setCookies.ts
+++ src/app/utils/setCookies.ts
@@ -5,42 +5,12 @@ export interface AuthTokens {
 };
 
 export const setCookies = (res: Response, token: AuthTokens) => {
-    // if (token.accessToken) {
-    //     res.cookie("accessToken", token.accessToken, {
-    //         httpOnly: true,
-    //         // secure: envVars.NODE_ENV === "production",
-    //         // secure: true,
-    //         // sameSite: "none"
-    //         secure: process.env.NODE_ENV === "production",
-    //         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
-    //     });
-    // }
-    // if (token.refreshToken) {
-    //     res.cookie("refreshToken", token.refreshToken, {
-    //         httpOnly: true,
-    //         // secure: envVars.NODE_ENV === "production",
-    //         // secure: true,
-    //         // sameSite: "none"
-    //         secure: process.env.NODE_ENV === "production",
-    //         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
-    //     });
-    // }
-
-    // if (token.accessToken) {
-    //     res.cookie("accessToken", token.accessToken, {
-    //         httpOnly: true,
-    //         secure: process.env.NODE_ENV === "production", // true only in prod
-    //         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    //         maxAge: 24 * 60 * 60 * 1000, // 1 day
-    //     });
-    // }
 
     if (token.accessToken) {
         res.cookie("accessToken", token.accessToken, {
             httpOnly: true,
-            secure: true,
-            sameSite: "none",
-            maxAge: 24 * 60 * 60 * 1000
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "none"
         });
     }
 };
