dd41ec57489bc849e1645843c0da89e8ce02feca
istiak19 <istiak15-14128@diu.edu.bd>
2025-10-17 08:37:00 +0600

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,11 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://istiak.vercel.app"],
+    origin: [
+      "http://localhost:3000",
+      "https://istiak.vercel.app",
+      "https://www.istiakdev.site"
+    ],
     credentials: true,
   })
 );
